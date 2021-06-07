8fb501516ac3d908156ba0f87b716f6d9ce60d65
josh.kent94 <josh.kent94@yahoo.co.uk>
2021-06-07 23:13:04 +0100

    Moving app file

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import AuthenticatedApp from './components/AuthenticatedApp';
-import Authentication from './features/Authentication/Authentication';
-import { selectAuthentication } from './features/Authentication/AuthenticationSlice';
+import AuthenticatedApp from '../components/AuthenticatedApp';
+import Authentication from '../features/Authentication/Authentication';
+import { selectAuthentication } from '../features/Authentication/AuthenticationSlice';
 
 function App() {
   const isAuthenticated = useSelector(selectAuthentication);
